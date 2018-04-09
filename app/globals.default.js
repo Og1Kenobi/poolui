47ebedaf3cddb3befaa2fc046d647d8378dcca83
Og1Kenobi <jon.r.douglas@me.com>
2018-04-09 07:19:06 +0000

    MakeOver

--- app/globals.default.js
+++ app/globals.default.js
@@ -4,9 +4,9 @@ angular.module('pool.globals', [])
 
 .factory('GLOBALS', function() {
 	return {
-		pool_name: "XMRPool.net",
-		api_url : 'https://api.xmrpool.net',
+		pool_name: "ACMEhash.com",
+		api_url : 'http://162.249.228.7/api',
 		api_refresh_interval: 5000,
-		app_update_interval: 30*60000
+		app_update_interval: 5*60000
 	};
 });
