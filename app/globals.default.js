fa743f78fe902772d11b792481a59e4352b056c3
Og1Kenobi <jon.r.douglas@me.com>
2018-04-10 02:32:37 +0000

    cosnetics

--- app/globals.default.js
+++ app/globals.default.js
@@ -5,7 +5,7 @@ angular.module('pool.globals', [])
 .factory('GLOBALS', function() {
 	return {
 		pool_name: "ACMEhash.com",
-		api_url : 'http://127.0.0.1/api',
+		api_url : 'http://acmehash.com/api',
 		api_refresh_interval: 5000,
 		app_update_interval: 5*60000
 	};
