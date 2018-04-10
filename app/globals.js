a9af7209e23db1e77642c5500e00fdcd42d9d200
Og1Kenobi <jon.r.douglas@me.com>
2018-04-10 00:18:06 +0000

    We're goin through changes.....

--- app/globals.js
+++ app/globals.js
@@ -5,7 +5,7 @@ angular.module('pool.globals', [])
 .factory('GLOBALS', function() {
 	return {
 		pool_name: "ACMEhash.com",
-		api_url : 'http://162.249.228.7/api',
+		api_url : 'http://acmehash.com/api',
 		api_refresh_interval: 5000,
 		app_update_interval: 5*60000
 	};
--- app/user/home/home.js
+++ app/user/home/home.js
@@ -6,7 +6,8 @@ app.controller('HomeCtrl', function($scope, $route, dataService, timerService) {
         dataService.getData("/pool/chart/hashrate/pplns", function(data){
             data = _.forEach(data, function(element) {
                 element.ts = new Date(element.ts);
-		element.hs = element.hs/1000000;
+/*		element.hs = element.hs/1000000; */
+		element.hs = element.hs/1;
             });
 
             $scope.poolHashrateChart = {
@@ -35,7 +36,7 @@ app.controller('HomeCtrl', function($scope, $route, dataService, timerService) {
 						 "dataset":"global",
 						 "label":"Total Pool Hashrate",
 						 "key":"hs",
-						 "color":"purple",
+						 "color":"#25f405",
 						 "type":["line","area"]}
                     ],
                     allSeries: [],
